31da506b1bc040f8e49018b7a6d06578de212c30
Mattias Velamsson <mattiaswelamsson@gmail.com>
2022-12-21 14:03:57 +0900

    added react fragments

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Card from "./Card";
+import Button from "./Button";
+import classes from "./ErrorModal.module.css";
+import Wrapper from "../Helpers/Wrapper";
+
+const ErrorModal = (props) => {
+  return (
+    <Wrapper>
+      <div className={classes.backdrop} onClick={props.onConfirm} />
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </Wrapper>
+  );
+};
+
+export default ErrorModal;
